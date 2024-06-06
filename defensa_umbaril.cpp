03ca38fdc1fff3f07b9c8e57c20c40b0f5e13be2
IsabelUrribarri <urribarriisabel@gmail.com>
2024-06-05 21:55:03 -0400

    terminado

--- defensa_umbaril.cpp
+++ defensa_umbaril.cpp
@@ -6,38 +6,36 @@ using namespace std;
 const float g = 9.81;
 
 void leerDatos(int &Xo, int &Yo, int &Vo, int &theta, int &M) {
-    cout << "Ingrese la posicion inicial (Xo Yo), velocidad inicial (Vo), angulo (theta) y numero de objetivos (M):\n";
     cin >> Xo >> Yo >> Vo >> theta >> M;
 
     // Validaciones de entrada
-    while (cin.fail() || Vo <= 0 || M <= 0) {
+    while (cin.fail() || Vo <= 0 || M <= 0 || theta < 0 || theta > 180) {
         cin.clear(); // Limpiar el estado de error
-        while (cin.get() != '\n') ; // Descartar la entrada inválida
-        cout << "Entrada invalida. Por favor, ingrese nuevamente:\n";
-        cin >> Xo >> Yo >> Vo >> theta >> M;
+        while (cin.get() != '\n'); // Descartar la entrada invalida
+        cout << "Datos de entrada invalidos\n";
+        exit(0);
     }
 }
 
-void calcularResultados(int Xo, int Yo, int Vo, int theta, int M) {
+void calcularResultados(int Xo, int Yo, int Vo, int theta, int M, char canonID) {
     float Vx = Vo * cos(theta * M_PI / 180.0);
     float Vy = Vo * sin(theta * M_PI / 180.0);
     float H = (Vy * Vy) / (2 * g);
     float T = 2 * Vy / g;
 
-    cout << "Los proyectiles subiran hasta " << round(H) << " metros antes de comenzar a caer.\n";
-    cout << "Estos impactaran contra el suelo pasados " << round(T) << " segundos luego de ser disparados.\n";
-    
-    for(int i = 0; i < M; ++i) {
+    cout << "Los proyectiles del canon " << canonID << " subiran hasta " << (int)round(H) << " metros antes de comenzar a caer.\n";
+    cout << "Estos impactaran contra el suelo pasados " << (int)round(T) << " segundos luego de ser disparados.\n";
+
+    for (int i = 0; i < M; ++i) {
         int X, Y;
-        cout << "Ingrese la posicion del objetivo " << (i + 1) << " (X Y):\n";
         cin >> X >> Y;
 
         // Validaciones de entrada
         while (cin.fail()) {
             cin.clear(); // Limpiar el estado de error
-            while (cin.get() != '\n') ; // Descartar la entrada inválida
-            cout << "Entrada invalida. Por favor, ingrese nuevamente:\n";
-            cin >> X >> Y;
+            while (cin.get() != '\n'); // Descartar la entrada invalida
+            cout << "Datos de entrada invalidos\n";
+            exit(0);
         }
 
         if (X == Xo && Y == Yo) {
@@ -50,34 +48,36 @@ void calcularResultados(int Xo, int Yo, int Vo, int theta, int M) {
         } else {
             float tImpacto = (X - Xo) / Vx;
             float yImpacto = Yo + Vy * tImpacto - 0.5 * g * tImpacto * tImpacto;
-            
-            if (round(yImpacto) == round(Y)) {
-                cout << "Objetivo " << (i + 1) << " destruido en " << round(tImpacto) << " segundos\n";
+
+            if ((int)round(yImpacto) == (int)round(Y)) {
+                cout << "Objetivo " << (i + 1) << " destruido por el canon " << canonID << " en " << (int)round(tImpacto) << " segundos\n";
             } else {
-                cout << "Reajuste de " << round(atan((Y - Yo) / (X - Xo)) * 180.0 / M_PI - theta) << " grados requerido\n";
+                float ajuste = atan((Y - Yo) / (float)(X - Xo)) * 180.0 / M_PI - theta;
+                cout << "Reajuste de " << (int)round(ajuste) << " grados requerido en el canon " << canonID << "\n";
             }
         }
     }
+    cout << "=" << endl;
 }
 
 int main() {
     int N;
-    cout << "Ingrese el numero de canones:\n";
     cin >> N;
 
-    // Validación de entrada
+    // Validacion de entrada
     while (cin.fail() || N <= 0) {
         cin.clear(); // Limpiar el estado de error
-        while (cin.get() != '\n') ; // Descartar la entrada inválida
-        cout << "Entrada invalida. Por favor, ingrese un numero valido de canones:\n";
-        cin >> N;
+        while (cin.get() != '\n'); // Descartar la entrada invalida
+        cout << "Datos de entrada invalidos\n";
+        exit(0);
     }
 
-    for(int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         int Xo, Yo, Vo, theta, M;
         leerDatos(Xo, Yo, Vo, theta, M);
-        calcularResultados(Xo, Yo, Vo, theta, M);
+        char canonID = 'A' + i;
+        calcularResultados(Xo, Yo, Vo, theta, M, canonID);
     }
-    
+
     return 0;
 }
