ecf82e3ece8e0d1777a85b33f6d7f81eaad2b092
IsabelUrribarri <urribarriisabel@gmail.com>
2024-06-05 22:00:02 -0400

    terminado

--- defensa_umbaril.cpp
+++ defensa_umbaril.cpp
@@ -6,12 +6,15 @@ using namespace std;
 const float g = 9.81;
 
 void leerDatos(int &Xo, int &Yo, int &Vo, int &theta, int &M) {
-    cin >> Xo >> Yo >> Vo >> theta >> M;
+    cout << "Ingrese la posicion inicial del canon (Xo Yo): ";
+    cin >> Xo >> Yo;
+    cout << "Ingrese la velocidad inicial (Vo) y el angulo (theta): ";
+    cin >> Vo >> theta;
+    cout << "Ingrese el numero de objetivos (M): ";
+    cin >> M;
 
     // Validaciones de entrada
-    while (cin.fail() || Vo <= 0 || M <= 0 || theta < 0 || theta > 180) {
-        cin.clear(); // Limpiar el estado de error
-        while (cin.get() != '\n'); // Descartar la entrada invalida
+    if (cin.fail() || Vo <= 0 || M <= 0 || theta < 0 || theta > 180) {
         cout << "Datos de entrada invalidos\n";
         exit(0);
     }
@@ -25,15 +28,14 @@ void calcularResultados(int Xo, int Yo, int Vo, int theta, int M, char canonID) 
 
     cout << "Los proyectiles del canon " << canonID << " subiran hasta " << (int)round(H) << " metros antes de comenzar a caer.\n";
     cout << "Estos impactaran contra el suelo pasados " << (int)round(T) << " segundos luego de ser disparados.\n";
-
+    
     for (int i = 0; i < M; ++i) {
         int X, Y;
+        cout << "Ingrese la posicion del objetivo " << (i + 1) << " (X Y): ";
         cin >> X >> Y;
 
         // Validaciones de entrada
-        while (cin.fail()) {
-            cin.clear(); // Limpiar el estado de error
-            while (cin.get() != '\n'); // Descartar la entrada invalida
+        if (cin.fail()) {
             cout << "Datos de entrada invalidos\n";
             exit(0);
         }
@@ -62,18 +64,18 @@ void calcularResultados(int Xo, int Yo, int Vo, int theta, int M, char canonID) 
 
 int main() {
     int N;
+    cout << "Ingrese el numero de canones a simular: ";
     cin >> N;
 
     // Validacion de entrada
-    while (cin.fail() || N <= 0) {
-        cin.clear(); // Limpiar el estado de error
-        while (cin.get() != '\n'); // Descartar la entrada invalida
+    if (cin.fail() || N <= 0) {
         cout << "Datos de entrada invalidos\n";
         exit(0);
     }
 
     for (int i = 0; i < N; ++i) {
         int Xo, Yo, Vo, theta, M;
+        cout << "Ingrese los datos del canon " << char('A' + i) << ":\n";
         leerDatos(Xo, Yo, Vo, theta, M);
         char canonID = 'A' + i;
         calcularResultados(Xo, Yo, Vo, theta, M, canonID);
